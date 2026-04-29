4a55e4a35a3150716e3d918d2538dc43db770283
satyam kumar <101463844+satyam-64136@users.noreply.github.com>
2026-04-29 18:36:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -142,4 +142,5 @@ if __name__ == "__main__":
     print("  Dehradun Urban Safety Heatmap — Flask Backend v2")
     print("  Running at: http://127.0.0.1:5000")
     print("=" * 55)
-    app.run(debug=True, port=5000)
+   if __name__ == "__main__":
+    app.run()
